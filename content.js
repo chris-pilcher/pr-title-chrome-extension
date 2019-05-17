3268b6a43e3578e5f2733160e6808cfb09adf52c
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2019-05-17 21:19:29 +1200

    Shipped it

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
-var addVulcanTitleGenerator = setInterval(function() {
-	// If the insert PR title button doesn't exist
+setInterval(() => {
+	// If the "insert PR title" button doesn't exist then add it in
 	if (!document.getElementById('customPRTitleGeneratorButton')) {
 		let titleDiv = document.querySelector('.ms-TextField-fieldGroup');
 
-		// Check if VS title element exists
 		if (titleDiv) {
 			let generateButton = document.createElement('button');
 			generateButton.id = 'customPRTitleGeneratorButton';
+			generateButton.title = 'Generate PR title';
 			generateButton.innerText = '🖖';
 
 			generateButton.addEventListener('click', () => {
@@ -15,10 +15,10 @@ var addVulcanTitleGenerator = setInterval(function() {
 				let fromBranchName = document.getElementsByClassName('vss-PickListDropdown--title-text')[1].innerText;
 				let toBranchName = document.getElementsByClassName('vss-PickListDropdown--title-text')[2].innerText;
 
-				titleInput.value = fromBranchName + ' into ' + toBranchName;
+				titleInput.value = fromBranchName + ' to ' + toBranchName;
 
-				//https://stackoverflow.com/questions/54137836/change-value-of-input-made-with-react-from-chrome-extension/54138182
-				titleInput.setAttribute('value', fromBranchName + ' into ' + toBranchName);
+				// https://stackoverflow.com/questions/54137836/change-value-of-input-made-with-react-from-chrome-extension/54138182
+				titleInput.setAttribute('value', fromBranchName + ' to ' + toBranchName);
 				titleInput.dispatchEvent(new Event('change', { bubbles: true }));
 				titleInput.dispatchEvent(new Event('blur', { bubbles: true }));
 			});
