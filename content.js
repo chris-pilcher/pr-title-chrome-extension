5a42ba43e918e2483b8077ed420fbd3adc8985f0
Chris Pilcher <7553472+chris-pilcher@users.noreply.github.com>
2019-05-20 10:43:04 +1200

    2 spaces

--- content.js
+++ content.js
@@ -1,41 +1,43 @@
 // Represents information from the page https://*.visualstudio.com/*/_git/*/pullrequestcreate*
 const page = {
-	get titleInput() {
-		return $('.vc-pullRequestCreate-title-container').find('input');
-	},
-	get sourceBranchName() {
-		return $.url().param('sourceRef');
-	},
-	get targetBranchName() {
-		return $.url().param('targetRef');
-	},
-	get isPullRequestCreatePath() {
-		return $.url().attr().path.endsWith('pullrequestcreate');
-	}
+  get titleInput() {
+    return $(".vc-pullRequestCreate-title-container").find("input");
+  },
+  get sourceBranchName() {
+    return $.url().param("sourceRef");
+  },
+  get targetBranchName() {
+    return $.url().param("targetRef");
+  },
+  get isPullRequestCreatePath() {
+    return $.url()
+      .attr()
+      .path.endsWith("pullrequestcreate");
+  }
 };
 
 // Updates the title of the pull request
 function updateTitle() {
-	page.titleInput.val(`${page.sourceBranchName} to ${page.targetBranchName}`);
+  page.titleInput.val(`${page.sourceBranchName} to ${page.targetBranchName}`);
 
-	// The follow two lines are required to change the value of an input made with reactjs.
-	// See: https://stackoverflow.com/questions/54137836/change-value-of-input-made-with-react-from-chrome-extension/54138182
-	page.titleInput[0].dispatchEvent(new Event('change', { bubbles: true }));
-	page.titleInput[0].dispatchEvent(new Event('blur', { bubbles: true }));
+  // The follow two lines are required to change the value of an input made with reactjs.
+  // See: https://stackoverflow.com/questions/54137836/change-value-of-input-made-with-react-from-chrome-extension/54138182
+  page.titleInput[0].dispatchEvent(new Event("change", { bubbles: true }));
+  page.titleInput[0].dispatchEvent(new Event("blur", { bubbles: true }));
 }
 
 // Called when changes are made to the DOM tree.
 function handleMutation() {
-	if (!page.isPullRequestCreatePath) return;
+  if (!page.isPullRequestCreatePath) return;
 
-	const generateButtonNotVisible = !$('#generatePRTitle').length;
-	if (generateButtonNotVisible) {
-		const generateButton = $('<button>🖖</button>')
-			.attr({ id: 'generatePRTitle', title: 'Generate PR title' })
-			.click(updateTitle);
+  const generateButtonNotVisible = !$("#generatePRTitle").length;
+  if (generateButtonNotVisible) {
+    const generateButton = $("<button>🖖</button>")
+      .attr({ id: "generatePRTitle", title: "Generate PR title" })
+      .click(updateTitle);
 
-		page.titleInput.after(generateButton);
-	}
+    page.titleInput.after(generateButton);
+  }
 }
 
 const observer = new MutationObserver(handleMutation);
